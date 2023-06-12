cc3c5d8387087484cc62d9bc8a31e6fcb394a493
Carsten Hennig <carsten.hennig@gmail.com>
2023-06-12 15:00:30 +0200

    Try to setup backend

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,52 @@
-export function handleClick(index) {
-  // Function logic remains the same
+let currentPlayer = "X";
+let winner = null;
+let board = ["", "", "", "", "", "", "", "", ""];
+let isDraw = false;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function handleClick(index) {
+  if (board[index] || winner || isDraw) return;
+
+  board[index] = currentPlayer;
+  checkWinner();
+
+  if (!board.includes("")) {
+    isDraw = true;
+    return;
+  }
+
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
 }
 
-export function resetGame() {
-  // Function logic remains the same
+function checkWinner() {
+  for (let combination of winningCombinations) {
+    const [a, b, c] = combination;
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      winner = board[a];
+      return;
+    }
+  }
 }
 
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-// export const prerender = true;
+function resetGame() {
+  currentPlayer = "X";
+  winner = null;
+  board = ["", "", "", "", "", "", "", "", ""];
+  isDraw = false;
+}
+
+module.exports = {
+  handleClick,
+  resetGame,
+};
--- src/routes/server.js
+++ src/routes/server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+const express = require("express");
+const app = express();
+const { handleClick, resetGame } = require("./public/api.js");
 
-// const express = require("express");
-// const app = express();
+app.use(express.static("public"));
 
-// app.use(express.static("public"));
+app.post("/api/click/:index", (req, res) => {
+  const index = req.params.index;
+  handleClick(index);
+  res.sendStatus(200);
+});
 
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
+app.post("/api/reset", (req, res) => {
+  resetGame();
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
